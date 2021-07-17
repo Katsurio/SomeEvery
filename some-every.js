b3605bd6bb62e6ee08d62e1891306c453ad5098b
John (Jef) Fisher <Fishe545@gmail.com>
2021-07-17 13:31:20 +0800

    all tests pass

--- some-every.js
+++ some-every.js
@@ -92,7 +92,9 @@ function hasCertainKey(arr, key) {
 //   ),
 // )
 /*
-Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
+Write a function called hasCertainValue which accepts an array of objects and a key, and a value, 
+and returns true if every single object in the array contains that value for the specific key. 
+Otherwise it should return false.
 
 Examples:
     var arr = [
@@ -107,4 +109,20 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+  return arr.every((objKey) => {
+    return objKey[key] === searchValue
+  })
+}
+console.log(
+  hasCertainValue(
+    [
+      { title: 'Instructor', first: 'Elie', last: 'Schoppik' },
+      { title: 'Instructor', first: 'Tim', last: 'Garcia', isCatOwner: true },
+      { title: 'Instructor', first: 'Matt', last: 'Lane' },
+      { title: 'Instructor', first: 'Colt', last: 'Steele', isCatOwner: true },
+    ],
+    'first',
+    'Elie',
+  ),
+)
